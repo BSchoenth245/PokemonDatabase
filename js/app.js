0593a42213a5eb8364396283699f66bd41c4c258
BSchoenth245 <brockschoenthaler@gmail.com>
2025-03-06 11:45:24 -0600

    added search page and began fillinf enter page

--- js/app.js
+++ js/app.js
@@ -1,16 +1,35 @@
 document.querySelector('#btnSwapMtE').addEventListener('click', (event) => {
-    // fetch ("components/SearchPokemon.html")
-    // .then(response => response.text())
-    // .then(html => {
+    fetch ("components/EnterPokemon.html")
+    .then(response => response.text())
+     .then(html => {
     //     const objScript = document.createElement('script');
     //     objScript.src = 'js/table.js'; 
     //     objScript.type = 'text/javascript';
     //     document.head.appendChild(objScript);
     //     objScript.onload = () => {
     //         fillTable(arrCourseInfo)
-    //       };
+    //     };
+        document.querySelector('#Content').innerHTML += html;
         $('#mainScreen').removeClass('d-flex')
         document.querySelector('#mainScreen').style.display = 'none';
         document.querySelector('#enterPkm').style.display = 'block';
     })
-//})
+})
+
+document.querySelector('#btnSwapMtS').addEventListener('click', (event) => {
+    fetch ("components/SearchPokemon.html")
+    .then(response => response.text())
+     .then(html => {
+    //     const objScript = document.createElement('script');
+    //     objScript.src = 'js/table.js'; 
+    //     objScript.type = 'text/javascript';
+    //     document.head.appendChild(objScript);
+    //     objScript.onload = () => {
+    //         fillTable(arrCourseInfo)
+    //     };
+        document.querySelector('#Content').innerHTML += html;
+        $('#mainScreen').removeClass('d-flex')
+        document.querySelector('#mainScreen').style.display = 'none';
+        document.querySelector('#searchPkm').style.display = 'block';
+    })
+})
